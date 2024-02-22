3f96f77321be8bf51f0fb125f907e314584226ce
johan-flowcode <moises.escudero.os@gmail.com>
2024-02-22 14:55:49 +0100

    creacion de admin

---
+++ admin/src/Components/Sidebar/Sidebar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import './Sidebar.css'
+import { Link } from "react-router-dom";
+
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <Link to={'/addproduct'} style={{textDecoration:"none"}}>
+        <div className="sidebar-item"> 
+        <i class="fa-solid fa-cart-shopping"></i>
+        <p>Add Product</p>
+        </div>
+      </Link>
+      <Link to={'/listproduct'} style={{textDecoration:"none"}}>
+        <div className="sidebar-item"> 
+        <i class="fa-solid fa-rectangle-list"></i>
+        <p>Product List</p>
+        </div>
+      </Link>
+    </div>
+  )
+}
+
+export default Sidebar
